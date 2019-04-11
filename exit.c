f379f233f5edbc43b7533e5df2a188d6513d8f76
Giovanny Perez <789@holbertonschool.com>
2019-04-11 15:28:25 +0000

    Draft builtin exit

---
+++ exit.c
@@ -0,0 +1,20 @@
+/**
+ *built_exit - exit from shell
+ *
+ *@status: exit status
+ *
+ *Return: pointer to string
+ */
+void built_exit(char *name, ...)
+{
+	va_list list;
+	va_start(list, name);
+
+
+
+
+
+
+
+
+}
