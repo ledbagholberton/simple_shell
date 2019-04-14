77363a9992e2575b3da8c308f0ccf1ac383a617f
Haroldo Vélez <hvelez020@gmail.com>
2019-04-14 08:29:13 +0000

    [delete] hidden printf

--- engine.c
+++ engine.c
@@ -36,7 +36,6 @@ void valid_command(char **argv, char *name, int line)
 	ful_path = _which(*argv);
 	if (ful_path != NULL)
 	{
-		printf("%o", argv[0][0]);
 		if (execve(ful_path, argv, NULL) == -1)
 			perror(name);
 		exit(EXIT_FAILURE);
