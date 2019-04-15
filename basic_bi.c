a1b9418928e64472dc9544a3d1583bf0dec88025
Giovanny Perez <789@holbertonschool.com>
2019-04-15 22:37:45 +0000

    Errores en CD

--- basic_bi.c
+++ basic_bi.c
@@ -53,6 +53,7 @@ int built_cd(char **cadena)
 void cd_parent(char **argv, char *name)
 {
 	char *home, buf[BUFSIZ], *cp, *chr;
+	static char *old_pwd;
 
 	cp = getcwd(buf, sizeof(buf));
 	home = get_home();
@@ -62,9 +63,11 @@ void cd_parent(char **argv, char *name)
 		chr = "";
 	if (argv[1] == NULL || chr[0] == '~' || chr[0] == '-')
 	{
+		printf("OLDPWD :%s, chr[0] %c /n", old_pwd, chr[0]);
+
 		if (chr[0] == '-')
 		{
-			chdir(cp);
+			chdir(old_pwd);
 		}
 		else if (chdir(home) == -1)
 		{
@@ -77,6 +80,8 @@ void cd_parent(char **argv, char *name)
 		chdir(cp);
 		perror(name);
 	}
+	old_pwd = cp;
+
 }
 
 /**
--- simple_shell.h
+++ simple_shell.h
@@ -66,4 +66,5 @@ int built_history(char **cadena);
 char *get_path(void);
 char *_strdup(char *str);
 int built_help(char **cadena);
+char *get_oldpwd(void);
 #endif
